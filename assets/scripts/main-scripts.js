1e297d0b6455c224907d4d59c0dcdc644e35ca53
Nathan Ryan <rockymountainshredder@gmail.com>
2021-10-08 15:19:46 -0600

    Portfolio Update

---
+++ assets/scripts/main-scripts.js
@@ -0,0 +1,11 @@
+
+const navigation = document.getElementById('nav-menu-icon');
+
+// function toggleMenu() {
+
+    // if (navigation.style.display === "block") {
+    //     navigation.style.display = "none";
+    // } else {
+    //     navigation.style.display = "block";
+    // }
+// }
