2d4434fbbf2e9c8485ff7e68a1cb723822af9f8d
Nathan Ryan <rockymountainshredder@gmail.com>
2021-11-11 21:42:12 -0700

    JavaScript comment removal

--- assets/scripts/main-scripts.js
+++ assets/scripts/main-scripts.js
@@ -1,9 +1,7 @@
 
 
 // Get the button:
-// const MYBUTTON = document.getElementById("scrollToTopBtn");
 let mybutton = document.getElementById("scrollToTopBtn");
-// let formButton = document.getElementById("btn-tattoo-review").addEventListener("click", displayForm);
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -21,18 +19,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-// function displayForm() {
-//   document.getElementById("book-tattoo-form").textContent = "Test Text";
-// }
-
-//const navigation = document.getElementById('nav-menu-icon');
-
-// function toggleMenu() {
-
-    // if (navigation.style.display === "block") {
-    //     navigation.style.display = "none";
-    // } else {
-    //     navigation.style.display = "block";
-    // }
-// }
