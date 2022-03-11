bc25c05be28a17657d236d63c2bfd85bc1b293e1
Nathan Ryan <rockymountainshredder@gmail.com>
2022-03-11 01:30:51 -0700

    Added event listener to scroll to top button, removed onclick call from index.html

--- assets/scripts/main-scripts.js
+++ assets/scripts/main-scripts.js
@@ -1,15 +1,17 @@
 
 // Get the button:
-let mybutton = document.getElementById("scrollToTopBtn");
+let scrollToTop = document.getElementById("scrollToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+scrollToTop.addEventListener('click', topFunction);
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    scrollToTop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    scrollToTop.style.display = "none";
   }
 }
 
